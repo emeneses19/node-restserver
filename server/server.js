7888a23e2a8af91a1b727814f24056c69a5db994
Meneses Tipe Epifania <menesetaipe19@gmail.com>
2019-12-28 19:51:12 -0500

    cambion 03

--- server/server.js
+++ server/server.js
@@ -1,35 +1,53 @@
-require('./config/config')
+require('./config/config');
+
 const express = require('express');
 const app = express();
-//body parser
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+
 
 app.get('/usuario', function(req, res) {
-    res.json('Hello World')
-})
+    res.json('get Usuario LOCAL!!!');
+});
+
 app.post('/usuario', function(req, res) {
+
     let body = req.body;
-    res.json({
-        persona: body
-    })
-})
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+
+});
+
 app.put('/usuario/:id', function(req, res) {
+
     let id = req.params.id;
+
     res.json({
         id
     });
-    //res.json('put Usuarios')
 });
+
 app.delete('/usuario', function(req, res) {
-    res.json('delete Usuarios')
+    res.json('delete Usuario');
 });
 
 app.listen(process.env.PORT, () => {
-    console.log('escuchando puerto:', 3000);
+    console.log('Escuchando puerto: ', process.env.PORT);
 });
